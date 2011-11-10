3cd3263f7d31991a9dcdefd681043c3be211e8c4
dush <dusanek@iquest.cz>
2011-11-10 17:52:08 +0100

    code cleanup

--- lib/generators/has_mysql_fulltext/install_generator.rb
+++ lib/generators/has_mysql_fulltext/install_generator.rb
@@ -1,21 +1,17 @@
 require 'rails/generators/active_record/model/model_generator'
 
-module HasMysqlFulltext 
+module HasMysqlFulltext
   class InstallGenerator  < ActiveRecord::Generators::ModelGenerator
-
     argument :name, :default => "fulltext_index"
-     
+    source_root File.expand_path("../templates", __FILE__)
 
-      source_root File.expand_path("../templates", __FILE__)
+    def copy_migration
+      create_migration_file
+    end
 
-      def copy_migration
-        create_migration_file
-      end
+    def generate_model
+      create_model_file
+    end
 
-      def generate_model
-        create_model_file
-      end
-      
-    
   end
 end
