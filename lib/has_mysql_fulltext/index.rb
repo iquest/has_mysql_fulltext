92d3105281ea7b53d7daf1404ef159f4091f7d1e
dush <dusanek@iquest.cz>
2011-11-22 15:55:07 +0100

    use asterisk expansion of each word in boolean mode match

--- lib/has_mysql_fulltext/index.rb
+++ lib/has_mysql_fulltext/index.rb
@@ -15,8 +15,8 @@ module HasMysqlFulltext
       #validates :indexable_id, :presence => true
       validates :indexable_attribute, :uniqueness => { :scope => [:indexable_id, :indexable_type]}
       
-      scope :match, lambda { |expr| where("MATCH(fulltext_indices.data) AGAINST (? IN BOOLEAN MODE)",  expr.strip) }
-      scope :attribute_match, lambda { |attr, expr| where("indexable_attribute = ?", attr).where("MATCH(fulltext_indices.data) AGAINST (? IN BOOLEAN MODE)",  expr.strip) }
+      scope :match, lambda { |expr| where("MATCH(fulltext_indices.data) AGAINST (? IN BOOLEAN MODE)",  prepare_expression(expr.strip)) }
+      scope :attribute_match, lambda { |attr, expr| where("indexable_attribute = ?", attr).where("MATCH(fulltext_indices.data) AGAINST (? IN BOOLEAN MODE)",  prepare_expression(expr.strip)) }
     end
   end
 end
--- lib/has_mysql_fulltext/index/class_methods.rb
+++ lib/has_mysql_fulltext/index/class_methods.rb
@@ -1,6 +1,9 @@
 module HasMysqlFulltext
 	module Index
 		module ClassMethods
+		  def prepare_expression(expr = "")
+		    expr.to_s.gsub(/[^\w\s]/, '').tr_s(" "," ").strip.split.uniq.map {|word| word + "*"}
+		  end
 
 		end
 	end
